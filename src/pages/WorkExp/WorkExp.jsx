fac1ed85100747f56456f3dbb774c93160df1352
Satish Sahu <98869512+satishsahu25@users.noreply.github.com>
2024-01-05 13:48:31 +0530

    Update WorkExp.jsx

--- src/pages/WorkExp/WorkExp.jsx
+++ src/pages/WorkExp/WorkExp.jsx
@@ -17,51 +17,51 @@ const WorkExp = () => {
         </h2>
         <hr />
 
-        {/* <VerticalTimeline
+        <VerticalTimeline
         lineColor='#178871'
-        > */}
-          {/* <VerticalTimelineElement
+        >
+           <VerticalTimelineElement
     
             className="vertical-timeline-element--work"
             contentStyle={{ background: "white", color: "black" }}
             contentArrowStyle={{ borderRight: "7px solid  white" }}
-            date="2021 - 2023"
+            date="2023 - Present"
             iconStyle={{ background: "#138781", color: "white" }}
             icon={<MdSchool />}
           >
             <h3 className="vertical-timeline-element-title">
-              Bachelors in Computer Science
+              System Engineer (Backend Developer)
             </h3>
-            <h4 className="vertical-timeline-element-subtitle">IIT Kharagpur</h4>
+            <h4 className="vertical-timeline-element-subtitle">Cessini Private Limited, Kolkata</h4>
           
           </VerticalTimelineElement> <VerticalTimelineElement
             className="vertical-timeline-element--work"
             contentStyle={{ background: "white", color: "black" }}
             contentArrowStyle={{ borderRight: "7px solid  white" }}
-            date="2021 - 2023"
+            date="Dec'2023 - Jan'2024"
             iconStyle={{ background: "#138781", color: "white" }}
             icon={<MdSchool />}
           >
             <h3 className="vertical-timeline-element-title">
-              Bachelors in Computer Science
+              JavaScript Developer Intern
             </h3>
-            <h4 className="vertical-timeline-element-subtitle">IIT Kharagpur</h4>
+            <h4 className="vertical-timeline-element-subtitle">Codespeedy Private Limited, Kolkata</h4>
           
           </VerticalTimelineElement> <VerticalTimelineElement
             className="vertical-timeline-element--work"
             contentStyle={{ background: "white", color: "black" }}
             contentArrowStyle={{ borderRight: "7px solid  white" }}
-            date="2021 - 2023"
+            date="Dec'2023 - Jan'2024"
             iconStyle={{ background: "#138781", color: "white" }}
             icon={<MdSchool />}
           >
             <h3 className="vertical-timeline-element-title">
-              Bachelors in Computer Science
+              Data Scientist
             </h3>
-            <h4 className="vertical-timeline-element-subtitle">IIT Kharagpur</h4>
+            <h4 className="vertical-timeline-element-subtitle">Psyliq, Bengaluru Karnataka</h4>
           
-          </VerticalTimelineElement> */}
-        {/* </VerticalTimeline> */}
+          </VerticalTimelineElement> 
+        </VerticalTimeline>
         </div>
     </div>
 
