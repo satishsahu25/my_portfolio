18d12bfa0be3627fa7d16d28c780e8015bd7c7fd
Satish Sahu <98869512+satishsahu25@users.noreply.github.com>
2024-09-15 14:51:48 +0530

    Update About.jsx

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -14,9 +14,7 @@ const About = () => {
           <div className="col-md-6 col-xl-6 col-lg-6 col-xs-12 about-content">
             <h1>About me</h1>
             <p>
-             Myself Satish Sahu, I am a final year undergraduate student at IIT Kharagpur.
-             My interest lies in developing complex websites and softwares. My principles of life are continuous learning, hardwork, patience, fail early and fail often. 
-             My hobbies include teaching, gymming, reading and travelling...
+             Myself Satish Sahu, I am a final year undergraduate student at IIT Kharagpur. A dedicated software engineer with a passion for crafting innovative solutions. Driven by a competitive spirit, I've honed my skills through rigorous competitive programming(currently working on too).  I'm particularly interested in exploring the intersection of software engineering and artificial intelligence, with a focus on full stack development, mobile app development, computer vision, OpenAI and specific AI areas, e.g., machine learning, deep learning, natural language processing. Beyond coding, I'm committed to maintaining a balanced lifestyle through fitness and exploring entrepreneurial ventures. Seeking opportunities to collaborate with talented individuals and contribute to groundbreaking projects.
             </p>
           </div>
         </div>
