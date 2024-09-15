f240e09f8864922218cd7c40de92223ed0c207a2
Satish Sahu <98869512+satishsahu25@users.noreply.github.com>
2024-09-15 14:53:38 +0530

    Update About.jsx

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
         <div className="row">
           <div className="col-md-6 col-xl-6 col-lg-6 col-xs-12 about-img">
            
-            <img src="https://res.cloudinary.com/codercloud/image/upload/v1693340468/satish_profimage_vrsdds.jpg"
+            <img src="https://res.cloudinary.com/codercloud/image/upload/v1726392175/459107118_535136522233779_2433225103441361271_n_bs5uw1.jpg"
               /> 
           </div>
           <div className="col-md-6 col-xl-6 col-lg-6 col-xs-12 about-content">
